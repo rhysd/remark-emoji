bd9f19094dd936b741a74a43147cd34e540e5d55
rhysd <lin90162@yahoo.co.jp>
2021-04-06 12:39:45 +0900

    fix indentation and blank lines

--- index.js
+++ index.js
@@ -16,13 +16,13 @@ function plugin(options) {
     const emoticonEnable = !!settings.emoticon;
 
     function getEmojiByShortCode(match) {
-        // find emoji by shortcode - full match or with-out last char as it could be from text e.g. :-), 
+        // find emoji by shortcode - full match or with-out last char as it could be from text e.g. :-),
         const iconFull = emoticon.find(e => e.emoticons.includes(match)); // full match
         const iconPart = emoticon.find(e => e.emoticons.includes(match.slice(0, -1))); // second search pattern
         const trimmedChar = iconPart ? match.slice(-1): '';
         const addPad = pad ? ' ': '';
         let icon = iconFull ?
-            iconFull.emoji + addPad: 
+            iconFull.emoji + addPad:
             iconPart && (iconPart.emoji + addPad +  trimmedChar);
         return icon || match;
     }
@@ -38,7 +38,6 @@ function plugin(options) {
     function transformer(tree) {
         visit(tree, 'text', function(node) {
             node.value = node.value.replace(RE_EMOJI, getEmoji);
-            
             if (emoticonEnable) {
                 node.value = node.value.replace(RE_SHORT, getEmojiByShortCode);
             }
