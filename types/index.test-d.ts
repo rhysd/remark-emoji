c153f6eb6214593f31b919c7ee708caa60a97401
rhysd <lin90162@yahoo.co.jp>
2024-04-09 00:52:24 +0900

    add more type tests for index.d.ts

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,21 +1,21 @@
 import { unified } from 'unified';
 import emoji, { type RemarkEmojiOptions } from './index';
-import { expectError } from 'tsd';
+import { expectError, expectAssignable } from 'tsd';
 
 unified().use(emoji);
 unified().use(emoji, {});
+expectError(unified().use(emoji, { thisOptionDoesNotExist: true }));
+
 unified().use(emoji, { padSpaceAfter: true });
 unified().use(emoji, { padSpaceAfter: false });
-
 expectError(unified().use(emoji, { padSpaceAfter: 1 }));
 
 unified().use(emoji, { emoticon: true });
 unified().use(emoji, { emoticon: false });
-
 expectError(unified().use(emoji, { emoticon: 1 }));
 
 unified().use(emoji, { accessible: true });
 unified().use(emoji, { accessible: false });
 expectError(unified().use(emoji, { accessible: 'true' }));
 
-unified().use(emoji, {});
+expectAssignable<RemarkEmojiOptions>({});
