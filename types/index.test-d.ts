b4f76c3d96f6a1a34562ee6df565de7e63c6e244
rhysd <lin90162@yahoo.co.jp>
2024-04-09 00:42:26 +0900

    migrate from dtslint to tsd

--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,22 +1,21 @@
 import { unified } from 'unified';
-import emoji, { type RemarkEmojiOptions } from 'remark-emoji';
+import emoji, { type RemarkEmojiOptions } from './index';
+import { expectError } from 'tsd';
 
 unified().use(emoji);
 unified().use(emoji, {});
 unified().use(emoji, { padSpaceAfter: true });
 unified().use(emoji, { padSpaceAfter: false });
 
-unified().use(emoji, { padSpaceAfter: 1 }); // $ExpectError
+expectError(unified().use(emoji, { padSpaceAfter: 1 }));
 
 unified().use(emoji, { emoticon: true });
 unified().use(emoji, { emoticon: false });
 
-unified().use(emoji, { emoticon: 1 }); // $ExpectError
+expectError(unified().use(emoji, { emoticon: 1 }));
 
 unified().use(emoji, { accessible: true });
 unified().use(emoji, { accessible: false });
+expectError(unified().use(emoji, { accessible: 'true' }));
 
-const opts: RemarkEmojiOptions = {};
-unified().use(emoji, opts);
-
-const invalidOpts: RemarkEmojiOptions = { accessible: 'true' }; // $ExpectError
+unified().use(emoji, {});
